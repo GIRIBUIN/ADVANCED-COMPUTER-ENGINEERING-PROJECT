5dcd6f53aa1746bab2bf9feb8eaf5537baa01c7a
DESKTOP\healt <healthnet@naver.com>
2025-11-20 19:58:03 +0900

    수정 : 로그인 상태 성공 메시지 출력 변경

--- src/app/auth.py
+++ src/app/auth.py
@@ -16,6 +16,6 @@ def login_user(user_name):
     """ 로그인: 사용자 로그인 """
     existing_user = db.find_user_by_name(user_name)
     if existing_user:
-        return {"status" : "error", "message" : "로그인을 성공했습니다.", "user_id" : existing_user['user_id']}
+        return {"status" : "success", "message" : "로그인을 성공했습니다.", "user_id" : existing_user['user_id']}
     else:
         return {"status" : "error", "message" : "존재하지 않는 사용자입니다."}
